e7fce83d4a612d690d77afc54ff39b9ef4fc93f9
1Sekon <128014026+1Sekon@users.noreply.github.com>
2024-06-27 21:13:51 +0700

    Build

--- src/newb/main.sh
+++ src/newb/main.sh
@@ -9,11 +9,12 @@
 #include "config.h"
 
 // Newb legacy functions
-#include "functions/tonemap.h"
 #include "functions/detection.h"
+#include "functions/tonemap.h"
+#include "functions/encustom.h"
+#include "functions/clouds.h"
 #include "functions/fog.h"
 #include "functions/sky.h"
-#include "functions/clouds.h"
 #include "functions/lighting.h"
 #include "functions/water.h"
 #include "functions/rain.h"
